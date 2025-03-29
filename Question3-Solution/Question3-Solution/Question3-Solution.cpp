5c380935224bd6449acaf5c543d20e6c45385a3b
Drasc001 <dtr2170@email.vccs.edu>
2025-03-29 11:46:32 -0400

    Question3-solution Answer

--- Question3-Solution/Question3-Solution/Question3-Solution.cpp
+++ Question3-Solution/Question3-Solution/Question3-Solution.cpp
@@ -1,20 +1,41 @@
-// Question3-Solution.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath> 
+#include <cstdlib>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    double principal, interestRate;
+    int compound;
+
+    //Interaction
+    std::cout << "Enter the initial principal amount: $";
+    std::cin >> principal;
+    std::cout << "Enter the interest rate as a decimal:";
+    std::cin >> interestRate;
+    std::cout << "Enter the number of times the interest is compounded per year: ";
+    std::cin >> compound;
+
+    //clear screen
+#ifdef _WIN32
+    system("CLS"); // Windows
+#else
+    system("clear"); // Linux/macOS
+#endif
+
+
+    //Calculation
+    double amount_in_savings = principal * pow(1 + (interestRate / compound), compound);
+    double interest = amount_in_savings - principal;
+
+    //Present Results
+    std::cout << "Interest Rate: " << interestRate << " %\n";
+    std::cout << "Times Compounded: " << compound << endl;
+    std::cout << "Principle:  $" << principal << endl;
+    std::cout << "Interest: $" << interest << endl;
+    std::cout << "Amount in Savings: $" << amount_in_savings << endl;
+
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
