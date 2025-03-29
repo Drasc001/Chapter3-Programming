966f7126b09a1d9836388c89d28feaa877d620c3
Drasc001 <dtr2170@email.vccs.edu>
2025-03-29 11:43:17 -0400

    Question2 Solution

--- Question2/Question2/Question2.cpp
+++ Question2/Question2/Question2.cpp
@@ -1,20 +1,15 @@
-// Question2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double randomnum1 = rand();
+	double randomnum2 = rand();
+
+	double answer = randomnum1 + randomnum2;
+
+	cout << randomnum1 << " + " << randomnum2 << "=";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.get();
+	cout << answer;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
